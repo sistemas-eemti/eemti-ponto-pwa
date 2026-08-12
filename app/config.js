77a30ed7781dd23d5c9dda6d66be092078e044eb
Rafael David Teixeira <rafael.david.teixeira@gmail.com>
2026-08-12 11:13:10 -0300

    Configure Cloudflare Worker URL

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 // Defina a URL pública do Worker depois de criá-lo no Cloudflare.
 window.PONTO_CONFIG = {
-  API_URL: 'https://SUBSTITUA-PELO-SEU-WORKER.workers.dev/api'
+  API_URL: 'https://eemti-ponto-api.sistemas-eemti.workers.dev/api'
 };
